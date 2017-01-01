882d1bd18043c7884e4ff89f6841a2485b3635aa
Melih Mucuk <melih.mucuk@bilyoner.com>
2017-01-01 23:03:39 +0300

    search, paths & writers added :tada:

--- hugo.go
+++ hugo.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const (
@@ -24,8 +25,8 @@ func NewHurriyetAPI(apiKey string) *HurriyetAPI {
 	}
 }
 
-func (h HurriyetAPI) GetArticles() ([]Article, error) {
-	data, err := h.request("articles")
+func (h HurriyetAPI) ListArticles(query *Query) ([]Article, error) {
+	data, err := h.request("articles", query)
 	if err != nil {
 		return nil, err
 	}
@@ -39,8 +40,8 @@ func (h HurriyetAPI) GetArticles() ([]Article, error) {
 	return articles, nil
 }
 
-func (h HurriyetAPI) GetArticleById(Id string) (Article, error) {
-	data, err := h.request("articles/" + Id)
+func (h HurriyetAPI) SingleArticle(Id string, query *Query) (Article, error) {
+	data, err := h.request("articles/"+Id, query)
 	if err != nil {
 		return Article{}, err
 	}
@@ -54,13 +55,112 @@ func (h HurriyetAPI) GetArticleById(Id string) (Article, error) {
 	return article, nil
 }
 
-func (h HurriyetAPI) request(url string) ([]byte, error) {
+func (h HurriyetAPI) Search(keyword string, query *Query) (Search, error) {
+	data, err := h.request("search/"+keyword, query)
+	if err != nil {
+		return Search{}, err
+	}
+
+	var search Search
+	if jsonErr := json.Unmarshal(data, &search); jsonErr != nil {
+		// check response for error
+		return Search{}, jsonErr
+	}
+
+	return search, nil
+}
+
+func (h HurriyetAPI) ListPaths(query *Query) ([]Path, error) {
+	data, err := h.request("paths", query)
+	if err != nil {
+		return nil, err
+	}
+
+	var paths []Path
+	if jsonErr := json.Unmarshal(data, &paths); jsonErr != nil {
+		// check response for error
+		return nil, jsonErr
+	}
+
+	return paths, nil
+}
+
+func (h HurriyetAPI) SinglePath(Id string, query *Query) (Path, error) {
+	data, err := h.request("paths/"+Id, query)
+	if err != nil {
+		return Path{}, err
+	}
+
+	var path Path
+	if jsonErr := json.Unmarshal(data, &path); jsonErr != nil {
+		// check response for error
+		return Path{}, jsonErr
+	}
+
+	return path, nil
+}
+
+func (h HurriyetAPI) ListWriters(query *Query) ([]Writer, error) {
+	data, err := h.request("writers", query)
+	if err != nil {
+		return nil, err
+	}
+
+	var writers []Writer
+	if jsonErr := json.Unmarshal(data, &writers); jsonErr != nil {
+		// check response for error
+		return nil, jsonErr
+	}
+
+	return writers, nil
+}
+
+func (h HurriyetAPI) SingleWriter(Id string, query *Query) (Writer, error) {
+	data, err := h.request("writers/"+Id, query)
+	if err != nil {
+		return Writer{}, err
+	}
+
+	var writer Writer
+	if jsonErr := json.Unmarshal(data, &writer); jsonErr != nil {
+		// check response for error
+		return Writer{}, jsonErr
+	}
+
+	return writer, nil
+}
+
+func (h HurriyetAPI) request(url string, query *Query) ([]byte, error) {
 	req, err := http.NewRequest("GET", h.baseUrl+url, nil)
 	req.Header.Add("apikey", h.apiKey)
 	req.Header.Add("accept", "application/json")
-	resp, err := h.client.Do(req)
 
-	resp.StatusCode
+	if query != nil {
+		q := req.URL.Query()
+		if len(query.Filter) > 0 {
+			q.Add("$filter", strings.Join(query.Filter, ","))
+		}
+
+		if len(query.Select) > 0 {
+			q.Add("$select", strings.Join(query.Select, ","))
+		}
+
+		if query.Top != "" {
+			q.Add("$top", query.Top)
+		}
+
+		if query.Skip != "" {
+			q.Add("$skip", query.Skip)
+		}
+
+		if query.S != "" {
+			q.Add("s", query.S)
+		}
+
+		req.URL.RawQuery = q.Encode()
+	}
+
+	resp, err := h.client.Do(req)
 
 	if err != nil {
 		return nil, err
--- model.go
+++ model.go
@@ -1,24 +1,47 @@
 package hugo
 
 import (
+	"strings"
 	"time"
 )
+
+type HurriyetTime struct {
+	time.Time
+}
+
+func (self *HurriyetTime) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), `"`)
+	t, err := time.Parse("2006-01-02T15:04:05", s)
+	if err != nil {
+		t, err = time.Parse(time.RFC3339, s)
+	}
+	self.Time = t
+	return
+}
+
+type Query struct {
+	Filter []string
+	Select []string
+	Top    string
+	Skip   string
+	S      string
+}
 
 type Article struct {
 	ID          string        `json:"Id"`
 	ContentType string        `json:"ContentType"`
-	CreatedDate time.Time     `json:"CreatedDate"`
+	CreatedDate HurriyetTime  `json:"CreatedDate"`
 	Description string        `json:"Description"`
 	Editor      string        `json:"Editor"`
 	Files       []File        `json:"Files"`
 	Path        string        `json:"Path"`
 	RelatedNews []interface{} `json:"RelatedNews"`
-	StartDate   time.Time     `json:"StartDate"`
+	StartDate   HurriyetTime  `json:"StartDate"`
 	Tags        []interface{} `json:"Tags"`
 	Text        string        `json:"Text"`
 	Title       string        `json:"Title"`
 	URL         string        `json:"Url"`
-	Writers     []interface{} `json:"Writers"`
+	Writers     []Writer      `json:"Writers"`
 }
 
 type Metadata struct {
@@ -29,4 +52,25 @@ type Metadata struct {
 type File struct {
 	FileURL  string   `json:"FileUrl"`
 	Metadata Metadata `json:"Metadata"`
+}
+
+type Writer struct {
+	ID          string       `json:"Id"`
+	Fullname    string       `json:"Fullname"`
+	ContentType string       `json:"ContentType"`
+	CreatedDate HurriyetTime `json:"CreatedDate"`
+	Files       []File       `json:"Files"`
+	Path        string       `json:"Path"`
+	URL         string       `json:"Url"`
+}
+
+type Search struct {
+	Count   int64     `json:"Count"`
+	Results []Article `json:"List"`
+}
+
+type Path struct {
+	ID    string `json:"Id"`
+	Path  string `json:"Path"`
+	Title string `json:"Title"`
 }
